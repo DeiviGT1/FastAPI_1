9d700d00c1be694ac251db0581ddffd52d404255
root <root@LAPTOP-PB4STVBA.localdomain>
2022-10-25 00:33:39 -0500

    request_respone_body

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI, Body
 import uvicorn
 
 app = FastAPI()
+
+# Modelos
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
     return {"Hello": "World"}
 
+#Request and response body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return Person
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
